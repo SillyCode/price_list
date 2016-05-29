bca1530b3fbb658ac74ea08458f9a7800dd28d04
Mark Malin <mark.malin@xorcom.com>
2016-05-29 19:44:49 +0300

    Load strings
    Don't do anything with them right now

--- includes/strings.php
+++ includes/strings.php
@@ -1,6 +1,9 @@
 <?php
 
-var_dump(__DIR__);
-// file_get_contents(__file__
+// Consider only en right now. It's not that I'm gonna translate it to any other language.
+foreach(glob(__DIR__."/../i18n/en/*.txt") as $strings) {
+	$contents = file_get_contents($strings);
+	//TODO: localize strings
+}
 
 ?>
