89a69e55ebed16497eabc5d86dc32b6f8cad9b54
Mark Malin <mark.malin@xorcom.com>
2016-05-30 16:46:59 +0300

    Updated CPBX and Asterisk page

--- pages/cpbx_astribank.php
+++ pages/cpbx_astribank.php
@@ -1,11 +1,16 @@
 <?php
 
-class cpbx_astribank extends page {
-
-	protected function body() {
-		$tpl = new template('cpbx_astribank');
-		$tpl->render();
-	}
+foreach(glob(dirname(__FILE__)."/../includes/*.php") as $filename) {
+	require_once($filename);
 }
+
+class cpbx_astribank {
+
+	public function __construct() {}
+	public function populate(template $tpl) {}
+}
+
+$tpl = new template('cpbx_astribank');
+$tpl->render();
 
 ?>
