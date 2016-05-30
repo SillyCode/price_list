de7add6f9c4e0a5cd63ea79af0eb130c0170a5fb
Mark Malin <mark.malin@xorcom.com>
2016-05-30 11:38:26 +0300

    Added lib class with buildpath method

--- includes/lib.php
+++ includes/lib.php
@@ -1,7 +1,20 @@
 <?php
 
-function show_page($page) {
-	include($page); //Parse the text
+class lib {
+
+	function show_page($page) {
+		include($page); //Parse the text
+	}
+
+	public static function buildpath() {
+			$arguments = func_get_args();
+			$first = array_shift($arguments);
+			$arguments = array_map(function($argument) {
+					return ltrim($argument, DIRECTORY_SEPARATOR);
+			}, $arguments);
+			array_unshift($arguments, $first);
+			return implode(DIRECTORY_SEPARATOR, $arguments);
+	}
 }
 
 ?>
