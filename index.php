332bed827c5c6f208dded69d5f7e5297cc2ebde5
Mark Malin <mark.malin@xorcom.com>
2016-05-30 16:46:34 +0300

    Removed unused code

--- index.php
+++ index.php
@@ -4,13 +4,6 @@ foreach(glob(dirname(__FILE__)."/includes/*.php") as $filename) {
 	require_once($filename);
 }
 
-function content() {
-	$requested_url = ($_SERVER['REQUEST_URI']);
-	$class = str_replace('_','+', strtolower($requested_url));
-	ob_start();
-	return ob_get_clean();
-}
-
 $tpl = new template('nav_menu');
 $tpl->render();
 
--- templates/cpbx_astribank.tpl
+++ templates/cpbx_astribank.tpl
@@ -1,6 +1,1 @@
-<?php
-	require_once(__DIR__."/pages/nav_bar.html");
-?>
-
 <html>
-</html>
